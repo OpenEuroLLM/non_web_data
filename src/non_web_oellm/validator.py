ba27c343c39adb80057fc5a4f01b89c20bda185f
Florian Philippe <florian.philippegm@gmail.com>
2026-08-11 12:33:18 +0200

    upload isl data + add licenses in validator

--- src/non_web_oellm/validator.py
+++ src/non_web_oellm/validator.py
@@ -4,12 +4,105 @@ import re
 import validators
 from datetime import datetime
 
-TOPICS = ["Agriculture", "Literature", "Linguistics", "Politics", "Economics", "Law", "Science", "Technology", "Medicine", "Education", "Culture", "Journalism", "Society", "History", "Religion", "Philosophy", "Psychology", "Sports", "Entertainment", "Sociology", "Business", "Environment", "Other", "Unknown", "Public Administration"]
-DATA_TYPES = ["tmx", "gz", "tar", "docx", "rar", "ppt", "pptx", "zip","vtt", "aac", "m4a", "ogg", "wav", "pdf", "rtf", "mp4", "mp3", "txt", "html", "docx", "xlsx", "csv", "json", "xml", "doc"]
-LICENSES = ["CC-BY-NC-ND-2.5-ES", "Public Domain Mark 1.0", "CC-BY-NC-SA-2.5-ES", "explicit open license", "CC-BY-SA-3.0", "GPL-2.0-only", "CC-BY-NC-SA-4.0", "AGPL-3.0-only", "GFDL-1.3", "LGPL-2.0-only", "CC-BY-NC-SA-3.0", "unspecified", "LGPL-2.0-or-later", "CC-BY-4.0", "CC-BY-SA-4.0", "Apache-2.0", "CC-BY-NC-ND-4.0", "BSD-2-Clause-FreeBSD", "LGPL-3.0-only", "GPL-3.0-or-later", "CC-BY-2.5", "CC-BY-3.0", "CC-BY-NC-ND-3.0", "CC-BY-NC-4.0", "GPL-3.0-only", "CC-BY-NC-SA-2.0", "LGPL-3.0-or-later", "CC-BY-NC-3.0", "AGPL-1.0-or-later", "CC0 1.0", "CC-BY-SA-2.0", "BSD-3-Clause", "BSD-4-Clause", "GPL", "GNU General Public License v3.0 (GPL-3.0)", "GPL v3", "GPL - 2.0"]
+TOPICS = [
+    "Agriculture",
+    "Literature",
+    "Linguistics",
+    "Politics",
+    "Economics",
+    "Law",
+    "Science",
+    "Technology",
+    "Medicine",
+    "Education",
+    "Culture",
+    "Journalism",
+    "Society",
+    "History",
+    "Religion",
+    "Philosophy",
+    "Psychology",
+    "Sports",
+    "Entertainment",
+    "Sociology",
+    "Business",
+    "Environment",
+    "Other",
+    "Unknown",
+    "Public Administration",
+]
+DATA_TYPES = [
+    "tmx",
+    "gz",
+    "tar",
+    "docx",
+    "rar",
+    "ppt",
+    "pptx",
+    "zip",
+    "vtt",
+    "aac",
+    "m4a",
+    "ogg",
+    "wav",
+    "pdf",
+    "rtf",
+    "mp4",
+    "mp3",
+    "txt",
+    "html",
+    "docx",
+    "xlsx",
+    "csv",
+    "json",
+    "xml",
+    "doc",
+]
+LICENSES = [
+    "Public Domain Mark 1.0",
+    "CC-BY-NC-SA-2.5-ES",
+    "explicit open license",
+    "CC-BY-SA-3.0",
+    "GPL-2.0-only",
+    "CC-BY-NC-SA-4.0",
+    "AGPL-3.0-only",
+    "GFDL-1.3",
+    "LGPL-2.0-only",
+    "CC-BY-NC-SA-3.0",
+    "unspecified",
+    "LGPL-2.0-or-later",
+    "CC-BY-4.0",
+    "CC-BY-ND-4.0",
+    "CC-BY-ND-3.0",
+    "CC-BY-SA-4.0",
+    "Apache-2.0",
+    "CC-BY-NC-ND-4.0",
+    "BSD-2-Clause-FreeBSD",
+    "LGPL-3.0-only",
+    "GPL-3.0-or-later",
+    "CC-BY-2.0",
+    "CC-BY-2.5",
+    "CC-BY-3.0",
+    "CC-BY-NC-ND-3.0",
+    "CC-BY-NC-4.0",
+    "GPL-3.0-only",
+    "CC-BY-NC-SA-2.0",
+    "LGPL-3.0-or-later",
+    "CC-BY-NC-3.0",
+    "AGPL-1.0-or-later",
+    "CC0 1.0",
+    "CC-BY-SA-2.0",
+    "BSD-3-Clause",
+    "BSD-4-Clause",
+    "GPL",
+    "GNU General Public License v3.0 (GPL-3.0)",
+    "GPL v3",
+    "GPL - 2.0",
+]
 RISK_SCALE = ["Low", "Medium", "High"]
 
-class Validation():
+
+class Validation:
     def __init__(self):
         self.topics = TOPICS
         self.data_types = DATA_TYPES
@@ -18,36 +111,50 @@ class Validation():
 
     def validate_metadata(self, metadata):
         validated_metadata = {
-            "LANGUAGE_CODE": self.validate_language_codes(metadata.get("LANGUAGE_CODE")),
+            "LANGUAGE_CODE": self.validate_language_codes(
+                metadata.get("LANGUAGE_CODE")
+            ),
             "MACRO_LANG": self.create_macro_language(metadata.get("LANGUAGE_CODE")),
             "SCRIPT_LANG": self.validate_script(metadata.get("SCRIPT_LANG")),
             "LANGUAGE": self.create_language_name(metadata.get("LANGUAGE_CODE")),
             "VARIANT": self.validate_free_text(metadata.get("VARIANT")),
             "TOPIC": self.validate_topic(metadata.get("TOPIC")),
             "DATA_TYPE": self.validate_data_type(metadata.get("DATA_TYPE")),
-            "SOURCE_ORGANIZATION": self.validate_free_text(metadata.get("SOURCE_ORGANIZATION")),
+            "SOURCE_ORGANIZATION": self.validate_free_text(
+                metadata.get("SOURCE_ORGANIZATION")
+            ),
             "LICENSE": self.validate_license(metadata.get("LICENSE")),
             "DOWNLOAD_SOURCE": self.validate_url(metadata.get("DOWNLOAD_SOURCE")),
-            "MIXED_LANGUAGES": self.validate_mixed_languages(metadata.get("MIXED_LANGUAGES")),
+            "MIXED_LANGUAGES": self.validate_mixed_languages(
+                metadata.get("MIXED_LANGUAGES")
+            ),
             "COMMENTARY": self.validate_free_text(metadata.get("COMMENTARY")),
-            "DATASET_NAME_OR_DESCRIPTION": self.validate_free_text(metadata.get("DATASET_NAME_OR_DESCRIPTION")),
-            "DATE_OF_IDENTIFICATION": self.validate_date(metadata.get("DATE_OF_IDENTIFICATION")),
+            "DATASET_NAME_OR_DESCRIPTION": self.validate_free_text(
+                metadata.get("DATASET_NAME_OR_DESCRIPTION")
+            ),
+            "DATE_OF_IDENTIFICATION": self.validate_date(
+                metadata.get("DATE_OF_IDENTIFICATION")
+            ),
             "CONTACT": self.validate_free_text(metadata.get("CONTACT")),
-            "SOURCE_IDENTIFICATION_URL": self.validate_url(metadata.get("SOURCE_IDENTIFICATION_URL")),
+            "SOURCE_IDENTIFICATION_URL": self.validate_url(
+                metadata.get("SOURCE_IDENTIFICATION_URL")
+            ),
             "LEGAL_COMMENTS": self.validate_free_text(metadata.get("LEGAL_COMMENTS")),
             "AUTHORS": self.validate_free_text(metadata.get("AUTHORS")),
             "RISK_SCALE": self.validate_free_text(metadata.get("RISK_SCALE")),
         }
-        
+
         # Preserve NAME and PATH if they exist
-        if 'NAME' in metadata: validated_metadata['NAME'] = metadata['NAME']
-        if 'PATH' in metadata: validated_metadata['PATH'] = metadata['PATH']
+        if "NAME" in metadata:
+            validated_metadata["NAME"] = metadata["NAME"]
+        if "PATH" in metadata:
+            validated_metadata["PATH"] = metadata["PATH"]
 
         errors = [key for key, value in validated_metadata.items() if value is False]
         if errors:
             return "errors occured in the following fields: " + ", ".join(errors)
         return validated_metadata
-    
+
     def validate_language_codes(self, lang_code):
         try:
             l = iso639.Lang(lang_code)
@@ -56,7 +163,7 @@ class Validation():
             return False
         except:
             return False
-    
+
     def create_macro_language(self, lang_code):
         try:
             l = iso639.Lang(lang_code)
@@ -64,33 +171,33 @@ class Validation():
             return macro.pt3 if macro else lang_code
         except:
             return lang_code
-        
+
     def validate_script(self, script_code):
         if not isinstance(script_code, str):
             return False
         script = pycountry.scripts.get(alpha_4=script_code)
         return script_code.lower() if script is not None else False
-    
+
     def create_language_name(self, lang_code):
         try:
             l = iso639.Lang(lang_code)
             return l.name
         except:
             return "not found"
-    
+
     def validate_topic(self, topic):
         return topic if topic in self.topics else False
-    
+
     def validate_data_type(self, data_type):
         return data_type if data_type in self.data_types else False
-    
+
     def validate_license(self, license):
         return license if license in self.licenses else False
-    
+
     def validate_url(self, url):
         validation = validators.url(url)
         return url if validation else False
-    
+
     def validate_mixed_languages(self, mixed_langs):
         if not isinstance(mixed_langs, list):
             return False
@@ -98,16 +205,16 @@ class Validation():
             if not self.validate_language_codes(lang):
                 return False
         return mixed_langs
-    
+
     def validate_date(self, date_str):
         try:
             datetime.strptime(date_str, "%d/%m/%Y")
             return date_str
         except (ValueError, TypeError):
             return False
-    
+
     def validate_free_text(self, text):
         return text if isinstance(text, str) else False
 
     def validate_risk(self, risk):
-            return risk if risk in self.risk_scale else False
+        return risk if risk in self.risk_scale else False
---
+++ temporary_database/isl/README.md
@@ -0,0 +1,28 @@
+# To download 
+
+4254 documents to scrape, of which : 
+
+## Licenses
+
+- 2915 unspecified licenses for which we should decide what to do
+- 1310 cc-by
+- 29 explicit open licenses
+
+## Data type
+
+- 4216 pdfs
+- 38 mp3s
+
+# Already downloaded
+
+5773 documents scraped by ELDA (we should decide how to exchange it, if required), of which : 
+
+## Licenses
+
+- 5725 unspecified licenses for which we should decide what to do
+- 8 cc-by
+
+## Data type
+
+- 5715 txts (some text extracted from pdfs, some from html)
+- 18 pdfs
